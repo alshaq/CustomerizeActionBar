6edc2165cff957055152f66287a388024db610c4
alshaq <orlandoshaquillelewis85@gmail.com>
2017-04-03 13:37:00 -0600

    Initial commit

---
+++ app/src/main/java/com/example/shaqu/customerizeactionbar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.shaqu.customerizeactionbar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //creating the toolbar variable , so the main activity can access the toolbar
+        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        //telling android to use the new created android
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id==R.id.navigate){
+            startActivity(new Intent(this,SubActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
